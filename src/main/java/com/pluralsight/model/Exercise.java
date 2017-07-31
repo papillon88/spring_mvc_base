745c2132d927376003a6e0817195e46521de02b3
papillon <devopreet.ch@gmail.com>
2017-07-31 14:33:35 +0530

    create exercise entity

--- src/main/java/com/pluralsight/model/Exercise.java
+++ src/main/java/com/pluralsight/model/Exercise.java
@@ -1,12 +1,27 @@
 package com.pluralsight.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Range;
 
-
+@Entity
 public class Exercise {
-	
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	@Range(min = 1, max = 120)
 	private int minutes;
 	
